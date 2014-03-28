2b27dc8eb2810b9d812ad562c8a1e5d70a8d114d
K-RKidd <kr248196@muhlenberg.edu>
2014-03-28 10:01:04 -0400

    Initial Commit

---
+++ Quiz6/Quiz6AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz6AppDelegate.h
+//  Quiz6
+//
+//  Created by Krystle on 3/28/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz6AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
